2a1f223246bfbeedb99e5b468206f77e5b3f15d9
UV <utkarshvj09@gmail.com>
2019-07-07 01:34:52 +0530

    heroku config push

--- public/js/app.js
+++ public/js/app.js
@@ -21,7 +21,7 @@ weatherForm.addEventListener('submit',(event)=>{
      messageOne.textContent='Loading......';
     messageTwo.textContent='';
    console.log('testing');
-   fetch('http://localhost:3000/weather?address='+location).then((response)=>{
+   fetch('/weather?address='+location).then((response)=>{
     console.log(response)  
 
 
